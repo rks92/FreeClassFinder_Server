b6cccf512676fbebe842e1958f8a68e2f8dee18d
rks92 <rahul.shardha@gmail.com>
2014-02-15 00:03:56 -0500

    Put SQL calls in a static class

---
+++ src/main/java/sqlHandler/SQLMainWrapper.java
@@ -0,0 +1,27 @@
+package sqlHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SQLMainWrapper {
+	
+	public static class SQLMain{
+		public SQLMain(){
+			Connection c = null;
+			try {
+				Class.forName("org.sqlite.JDBC");
+				c = DriverManager.getConnection("jdbc:sqlite:/Volumes/Data/Databases/ClassFinder/test.db");
+			} catch (Exception e) {
+				System.err.println(e.getClass().getName() + ": " + e.getMessage());
+				System.exit(0);
+			}
+		}	
+		//Function to create the tables
+		
+		//Function to fill the tables
+		
+		//Function to read the tables
+		
+		//Querying function
+	}
+}
