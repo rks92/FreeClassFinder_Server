0d6e9aa8e6a7e9b21ea7ab494245cd0f47e13f58
rks92 <rahul.shardha@gmail.com>
2014-02-15 16:23:32 -0500

    Connection for the class to use

--- src/main/java/sqlHandler/SQLMainWrapper.java
+++ src/main/java/sqlHandler/SQLMainWrapper.java
@@ -6,11 +6,13 @@ import java.sql.DriverManager;
 public class SQLMainWrapper {
 	
 	public static class SQLMain{
+		private static Connection c;
 		public SQLMain(){
 			Connection c = null;
 			try {
 				Class.forName("org.sqlite.JDBC");
 				c = DriverManager.getConnection("jdbc:sqlite:/Volumes/Data/Databases/ClassFinder/test.db");
+				this.c = c;
 			} catch (Exception e) {
 				System.err.println(e.getClass().getName() + ": " + e.getMessage());
 				System.exit(0);
